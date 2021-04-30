c3c946fce4836c5a7221e77f829e36013aeeca9d
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2021-04-30 15:27:05 +1200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const lava = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
         mySprite.vy += -130
@@ -12,7 +15,6 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairEast, function (spri
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
     game.over(false)
 })
-let mySprite2: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     f f f f f f f f 
@@ -30,16 +32,3 @@ controller.moveSprite(mySprite, 100, 0)
 tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairWest)
 scene.cameraFollowSprite(mySprite)
 mySprite.ay = 200
-game.onUpdateInterval(5000, function () {
-    for (let index = 0; index < 100; index++) {
-        mySprite2 = sprites.create(img`
-            2 5 
-            4 2 
-            `, SpriteKind.Player)
-        if (Math.percentChance(50)) {
-            mySprite2.vy = randint(-30, -80)
-        } else {
-            mySprite2.vy = randint(-150, -200)
-        }
-    }
-})
